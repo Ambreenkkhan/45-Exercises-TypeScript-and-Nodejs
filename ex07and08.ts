000642b8a0681382c4766fff3e97f3b7182964a3
Ambreenkkhan <ambreenk2007@gmail.com>
2024-08-13 22:57:57 +0500

    second commit

---
+++ ex07and08.ts
@@ -0,0 +1,15 @@
+// 7.  Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+// 8. You should create four lines that look like this:
+// console.log(5 + 3)
+
+// Your output should simply be four lines with the number 8 appearing once on each line.
+
+//Addition
+console.log(4 + 4);
+//Subtraction
+console.log(15 - 7);
+//Multiplication
+console.log(4 * 2);
+//Division
+console.log(80 / 10);
